0fafa99a7ad6316293ca363238b93d756b8fd214
saji <saji@gmail.com>
2026-03-16 14:11:53 +0000

    Keypair created

---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "aws_region" {
+
+  description = "my aws region"
+  type        = string
+}
+
+variable "project_name" {
+
+  description = "my project name"
+  type        = string
+}
+
+variable "project_environment" {
+
+  description = "my project environment"
+  type        = string
+}
+
